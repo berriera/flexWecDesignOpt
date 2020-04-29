a483def0eb5b2946dc3be3cee6809fb484bbee64
Austin Berrier <berriera@oregonstate.edu>
2020-04-28 20:34:34 -0700

    Update main.py
    Initial attempt on running wamit through command line.
    Added space for comments.

--- flexWecDesignOpt/main.py
+++ flexWecDesignOpt/main.py
@@ -19,6 +19,10 @@ args = parser.parse_args(sys.argv[1:])
 
 
 def parse_input():
+    """
+
+    :return:
+    """
     import yaml
     with open('C:/Users/13365/Documents/GitHub/flexWecDesignOpt/flexWecDesignOpt/examples/barge/input.yaml', 'r') as f:
         file_names = yaml.safe_load(f)
@@ -26,6 +30,12 @@ def parse_input():
 
 
 def create_case_directory(case_number, output_directory):
+    """
+
+    :param case_number:
+    :param output_directory:
+    :return:
+    """
     import os
     path = output_directory + '/case_' + str(case_number)
     os.makedirs(path)
@@ -33,6 +43,12 @@ def create_case_directory(case_number, output_directory):
 
 
 def create_case_files(common_bem_file_folder, copy_folder):
+    """
+
+    :param common_bem_file_folder:
+    :param copy_folder:
+    :return:
+    """
     import shutil
     import os
     files = os.listdir(common_bem_file_folder)
@@ -42,12 +58,26 @@ def create_case_files(common_bem_file_folder, copy_folder):
     return
 
 
-def run_wamit():
-    # subprocess.run("C:\WAMITv7", "runwamit", "test01"]) # can also try subprocess.Popen
+def run_wamit(output_folder, bem_command):
+    """
+    This function runs the boundary element solver WAMIT in the current case directory.
+    :param output_folder:
+    :param bem_command:
+    :return:
+    """
+    import os
+    import subprocess
+    os.chdir(output_folder)
+    # subprocess.Popen()
+    subprocess.run(bem_command)
     return
 
 
 def read_output():
+    """
+
+    :return:
+    """
     return
 
 
@@ -62,7 +92,7 @@ for case in range(design_count):
     print(design_variables)
     case_output_folder = create_case_directory(case + 1, input_file_names['output_directory'])
     create_case_files(input_file_names['common_file_directory'], case_output_folder)
-    run_wamit(case_output_folder, input_file_names['run_wamit_directory'])
+    # run_wamit(case_output_folder, input_file_names['run_wamit_directory'])
     read_output()
 
 # if __name__ == "__main__":
