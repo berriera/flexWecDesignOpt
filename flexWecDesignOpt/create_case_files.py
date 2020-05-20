b19ae6f58ce4bd7cd95b155a16887914d134e111
Austin Berrier <berriera@oregonstate.edu>
2020-05-19 23:22:31 -0700

    Fixed and reorganized create_case_files.py
    - Code was breaking when dealing with more than one substitution in a line; now fixed
    - Should now support dictionaries from the device substitution function
    - Reorganized into three supporting functions

--- flexWecDesignOpt/create_case_files.py
+++ flexWecDesignOpt/create_case_files.py
@@ -1,3 +1,35 @@
+def string_change(line_change, variables, file_name='', line_number=1):  # TODO: documentation
+    while line_change.find('?') != -1:
+        substitution_indices = [position for position, character in enumerate(line_change) if character == '?']
+        if len(substitution_indices) % 2 != 0.0:
+            raise ValueError("Check line number" + str(line_number) + "in file" + file_name +
+                             "for proper substitution formatting")
+        string_substitution = line_change[substitution_indices[0]:substitution_indices[1] + 1]
+        if isinstance(variables, list):
+            replacement_variable_number = int(line_change[substitution_indices[0] + 1: substitution_indices[1]])
+            replacement_string = str(variables[replacement_variable_number - 1])
+        elif isinstance(variables, dict):
+            replacement_variable_key = line_change[substitution_indices[0] + 1: substitution_indices[1]]
+            replacement_string = line_change[replacement_variable_key]
+        else:
+            raise TypeError("Returned object type of device substitution should be an array or a dictionary.")
+        line_change = line_change.replace(string_substitution, replacement_string)
+    return line_change
+
+
+def change_case_file(text_file, design_var):  # TODO: documentation
+    with open(text_file, 'r') as g:
+        line_list = []
+        line_number = 0
+        lines_text = g.readlines()
+        for line in lines_text:
+            if line.find('?') != -1:
+                line = string_change(line, design_var)
+            line_list.append(line)
+            line_number += 1
+        return line_list
+
+
 def create_case_files(common_file_directory, case_output_folder, substitution_array):
     """Copies original input files from their common directory, and changes them in the process using variable
     substitution.
@@ -11,44 +43,14 @@ def create_case_files(common_file_directory, case_output_folder, substitution_ar
         None
     """
 
-    def change_case_file(text_file, design_var):
-        with open(text_file, 'r') as g:
-            line_list = []
-            line_number = 0
-            lines_text = g.readlines()
-            for line in lines_text:
-                if line.find('?') != -1:
-                    index = 0
-                    replace_keys = []
-                    for character in line:
-                        if character == '?':
-                            replace_keys.append(index)
-                        index += 1
-                    replace_count = int(len(replace_keys) / 2)
-                    for replacement in range(0, replace_count):
-                        string_beginning = replace_keys[replacement]
-                        string_end = replace_keys[replacement + 1]
-                        replacement_variable_number = int(line[string_beginning + 1:string_end])
-                        print(replacement_variable_number)
-                        old_string = line[string_beginning:string_end + 1] # TODO: figure out why multiple substitutions in a line breaks everything
-                        print('old_string')
-                        print(old_string)
-                        replacement_string = str(design_var[replacement_variable_number - 1])
-                        print('replacement_string')
-                        print(replacement_string)
-                        line = line.replace(old_string, replacement_string)
-                line_list.append(line)
-                line_number += 1
-            return line_list
-
     import shutil
     import os
     files = os.listdir(common_file_directory)
     for bem_input_file in files:
-        file = common_file_directory + '/' + bem_input_file
+        file = common_file_directory + '/' + bem_input_file  # TODO: exclude .csv files
         new_file_text = change_case_file(file, substitution_array)
         shutil.copy(file, case_output_folder)
-        change_file = case_output_folder + '/' + bem_input_file
+        change_file = case_output_folder + '/' + bem_input_file  # TODO: join these strings in a non-Windows specific way
         with open(change_file, 'w') as f:
             for new_string in new_file_text:
                 f.write(new_string)
