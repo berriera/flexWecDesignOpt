fb0083404dedb7cf08aa418c96ae31fd80d8a4a7
Austin Berrier <berriera@oregonstate.edu>
2020-05-27 21:37:57 -0700

    Example input file reorganization

---
+++ flexWecDesignOpt/examples/barge/high_order_meshing/flexible_barge_example.py
@@ -0,0 +1,13 @@
+# input yaml vars at top
+
+# compute m, k matrices with numpy integration
+
+# substitution classes
+
+device_name = 'FlexibleBarge'
+run_wamit_command = 'C:\WAMITv7\wamit'
+common_file_directory = 'C:/Users/13365/Documents/GitHub/flexWecDesignOpt/flexWecDesignOpt/examples/barge/high_order_meshing'
+output_directory = 'C:/Users/13365/Desktop/barge_output_test'
+cases_file = 'C:/Users/13365/Documents/GitHub/flexWecDesignOpt/flexWecDesignOpt/examples/barge_array.csv'
+gmsh_exe_location = 'C:/Users/13365/Documents/gmsh-4.5.6-Windows64/gmsh-4.5.6-Windows64/gmsh' # take these away
+mesh_refinement_factor = 0.25 # take this away
