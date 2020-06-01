b5c9632393f414fba218b0967ed08db3c49a7367
Austin Berrier <berriera@oregonstate.edu>
2020-06-01 00:37:03 -0700

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flexWecDesignOpt',
+    version='0.1.0',
+    description='Automates WAMIT response analysis for use with wave energy converter structural design optimization',
+    author='Austin Berrier',
+    author_email='austin.berrier@protonmail.com',
+    license='BSD-3-Clause',
+    python_requires='>=3',
+    package=find_packages()
+)
