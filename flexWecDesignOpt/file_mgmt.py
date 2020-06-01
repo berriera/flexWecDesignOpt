87ff15c686b2bcf24c84045b2c3ea9ad10b1d946
Austin Berrier <berriera@oregonstate.edu>
2020-06-01 00:40:22 -0700

    Reorganized file mgmt functions into common module

--- flexWecDesignOpt/file_mgmt.py
+++ flexWecDesignOpt/file_mgmt.py
@@ -12,3 +12,18 @@ def create_case_directory(case_number, output_directory):
     path = os.path.join(output_directory, 'case_' + str(case_number))
     os.makedirs(path)
     return path
+
+
+def parse_input(input_file_location):
+    """Opens and reads in specified parameters in the user created .yaml file
+
+    Args:
+        input_file_location (str):
+
+    Returns
+        str
+    """
+    import yaml
+    with open(input_file_location, 'r') as f:
+        file_names = yaml.safe_load(f)
+    return file_names
