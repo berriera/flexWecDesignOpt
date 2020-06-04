07d06a60855edbf0c5cb122c5ee62e5e4ead338a
Austin Berrier <berriera@oregonstate.edu>
2020-06-03 19:48:26 -0700

    Update create_case_directory to check for existing folders

--- flexWecDesignOpt/file_mgmt.py
+++ flexWecDesignOpt/file_mgmt.py
@@ -1,4 +1,4 @@
-def create_case_directory(case_number, output_directory):
+def create_case_directory(output_directory='', case_number=1):
     """Creates a directory to hold each design's input and generated output files.
 
     Args:
@@ -9,9 +9,22 @@ def create_case_directory(case_number, output_directory):
         str
     """
     import os
-    path = os.path.join(output_directory, 'case_' + str(case_number))
-    os.makedirs(path)
-    return path
+    if case_number >= 0:
+        file_path_list = os.listdir(output_directory)
+        folder_count = len(file_path_list)
+        directory_path = os.path.join(output_directory, 'case_' + str(case_number))
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+        else:
+            case_number = folder_count + 1
+            print('\tCase output folder already exists. Renaming to case_' + str(case_number))
+            directory_path = os.path.join(output_directory, 'case_' + str(case_number))
+            os.makedirs(directory_path)
+    return directory_path
+
+
+def current_case_directory():
+    return create_case_directory(case_number=-1)
 
 
 def parse_input(input_file_location):
