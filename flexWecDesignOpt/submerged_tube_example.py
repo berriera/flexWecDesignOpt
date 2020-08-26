12e66e43ad91a0de2f26d4af0d504bb464e6507d
Austin Berrier <berriera@oregonstate.edu>
2020-08-25 18:34:43 -0700

    Update submerged_tube_example.py
    - Created class structure for tube
    - Finished initial setup for running an example case

--- flexWecDesignOpt/submerged_tube_example.py
+++ flexWecDesignOpt/submerged_tube_example.py
@@ -13,21 +13,37 @@ output_directory = os.path.abspath(os.path.join('examples', 'output'))
 device_name = 'tube'
 mesh_refinement_factor = 0.40
 
-#common_file_directory = os.path.abspath(common_file_directory)
-#output_directory = os.path.abspath(output_directory)
+
+class FlexibleTube(object):
+
+    def __init__(self, design_vars):
+        self.device_name = 'tube'
+        self.depth = design_vars[0]
+        self.radius = design_vars[1]
+        self.thickness = design_vars[2]
+        self.length = design_vars[3]
+        self.distensibility = design_vars[4]
+        self.eta = design_vars[5]
+
+    def substitutions(self):
+        return {'z_s': self.depth, 'r_s': self.radius, 't': self.thickness, 'L': self.length
+                }
+
+    def geometry(self):
+        geometry = pygmsh.opencascade.Geometry()
+        geometry.add_cylinder(x0=[-self.length / 2, 0, self.depth], axis=[self.length, 0, 0],
+                              radius=self.radius, angle=2 * np.pi, char_length=1)
+        return geometry
+
 
 # Design variables
-depth = -1
-radius = 0.274
-thickness = 0.01
-length = 10
-end_cap_thickness = thickness
-
-geometry = pygmsh.opencascade.Geometry()
-outer_tube = geometry.add_cylinder(x0=[-length / 2, 0, depth], axis=[length, 0, 0], radius=radius,
-                                   angle=2 * np.pi, char_length=1)
-
+print('Case: ', str(1))
+design_variables = np.asarray([-1, 0.274, 0.01, 10, 0, 0])
+tube = FlexibleTube(design_variables)
+tube_substitutions = tube.substitutions()
+tube_geometry = tube.geometry()
 create_case_directory(output_directory, 1)
-create_mesh_file(geometry, device_name, gmsh_exe_location, mesh_refinement_factor)
+create_mesh_file(tube_geometry, device_name, gmsh_exe_location, mesh_refinement_factor)
 submerged_mesh(device_name)
 run_wamit(run_wamit_command, flexible_bool=True)
+print('Done.')
