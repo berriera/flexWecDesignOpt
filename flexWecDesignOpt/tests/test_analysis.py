d85ccd95380c0127e7dc2f7c78f814b1b946b0ca
Austin Berrier <berriera@oregonstate.edu>
2020-10-02 11:08:10 -0700

    Update test_analysis.py
    - Added boundary condition frequency tests for modal frequencies of a submerged flexible tube
    - Updated linear equation with a passed extra argument

--- flexWecDesignOpt/tests/test_analysis.py
+++ flexWecDesignOpt/tests/test_analysis.py
@@ -3,8 +3,8 @@ import math
 import numpy as np
 
 
-def linear_equation(x):
-    return 2 * (x - 1)
+def linear_equation(x, a):
+    return 2 * (x - a)
 
 
 def quadratic_equation(x):
@@ -23,15 +23,37 @@ def free_free_barge_equation(x):
     return math.cos(x) * math.cosh(x) - 1
 
 
+def flexible_tube_mode_type_1(w, L, Di, Ts, rho):
+    lk = math.sqrt(((2 * np.pi) / (Di * Ts)) * (math.sqrt(1 + (Ts * rho * (Di ** 2) * (w ** 2) / math.pi)) - 1))
+    uk = math.sqrt(((2 * np.pi) / (Di * Ts)) * (math.sqrt(1 + (Ts * rho * (Di ** 2) * (w ** 2) / math.pi)) + 1))
+    return (lk * L / 2) * math.tanh(uk * L / 2) - (uk * L / 2) * math.tan(lk * L / 2)
+    # TODO: look into if fabs from Jennifer's code is ABSOLUTEly necessary
+
+
+def flexible_tube_mode_type_2(w, L, Di, Ts, rho, r_s, M, K_a):
+    S_s = math.pi * (r_s ** 2)
+    lk = math.sqrt(((2 * np.pi) / (Di * Ts)) * (math.sqrt(1 + (Ts * rho * (Di ** 2) * (w ** 2) / math.pi)) - 1))
+    uk = math.sqrt(((2 * np.pi) / (Di * Ts)) * (math.sqrt(1 + (Ts * rho * (Di ** 2) * (w ** 2) / math.pi)) + 1))
+    return (uk * L / 2) * math.tanh(uk * L / 2) + (lk * L / 2) * math.tan(lk * L / 2) \
+           - (((w ** 2) * rho * S_s * L) / (-M * (w ** 2) + 2 * K_a)) * (uk / lk + lk / uk) \
+           * math.tanh(uk * L / 2) * math.tan(lk * L / 2)
+
+
 def test_0_roots_linear():
     exp = []
     obs = boundary_condition_frequency_solver(linear_equation, 0)
     assert np.allclose(exp, obs)
 
 
-def test_1_roots_linear():
+def test_1_roots_linear__1():
     exp = [1]
-    obs = boundary_condition_frequency_solver(linear_equation, 1)
+    obs = boundary_condition_frequency_solver(linear_equation, 1, extra_args=1)
+    assert np.allclose(exp, obs)
+
+
+def test_1_roots_linear__2():
+    exp = [math.pi]
+    obs = boundary_condition_frequency_solver(linear_equation, 1, extra_args=math.pi)
     assert np.allclose(exp, obs)
 
 
@@ -63,3 +85,16 @@ def test_4_roots_flexible_barge():
     exp = [4.7300, 7.8532, 10.9956, 14.1372]
     obs = boundary_condition_frequency_solver(free_free_barge_equation, 4)
     assert np.allclose(exp, obs)
+
+
+def test_5_roots_flexible_tube__mode_type_1():
+    exp = [2.1114360764249511, 4.5933899504176336, 7.7089354146509326, 11.6086922395909120, 16.3723249238653743]
+    obs = boundary_condition_frequency_solver(flexible_tube_mode_type_1, 5, extra_args=(10, 1.12e-4, 1.8770e4, 1000))
+    assert np.allclose(exp, obs)
+
+
+def test_5_roots_flexible_tube__mode_type_2():
+    exp = [0.5489434835683205, 1.4244105181770936, 1.9121722822149714, 3.9035022567988560, 6.6024612335389854]
+    obs = boundary_condition_frequency_solver(flexible_tube_mode_type_2, 5, h=0.5, freq_limit=8,
+                                              extra_args=(10, 1.12e-4, 1.8770e4, 1000, 0.274, 311.7, 510))
+    assert np.allclose(exp, obs, rtol=1e-2)
