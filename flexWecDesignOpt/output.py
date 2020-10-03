3a0ccf1e10556f1ba772a579cba732189880cbef
Austin Berrier <berriera@oregonstate.edu>
2020-10-02 22:44:28 -0700

    Update output.py
    Added option to write dictionary of design variables or variable substitutions to a text file

--- flexWecDesignOpt/output.py
+++ flexWecDesignOpt/output.py
@@ -4,3 +4,10 @@ def read_output():
     :return:
     """
     return
+
+
+def write_dict_to_text_file(dictionary, file_name='design_variables.txt'):
+    f = open(file_name, 'w')
+    for key in dictionary.keys():
+        f.write('{}={}\n'.format(key, dictionary[key]))
+    f.close()
