4928a3a893ad5b1624f186962920067e4bc2e0b9
Austin Berrier <berriera@oregonstate.edu>
2021-01-15 20:33:49 -0800

    Update file_mgmt.py
    - Added an analysis argument to create_case_directory function
    - Generates required subdirectories depending on specified analysis type
    - Created file_information function that returns subdirectory information in dict format and relevant input file extensions to copy depending on the specified analysis type

--- flexWecDesignOpt/file_mgmt.py
+++ flexWecDesignOpt/file_mgmt.py
@@ -1,9 +1,10 @@
-def create_case_directory(output_directory='', folder_name='design_case_'):
-    """Creates a directory to hold each design's input and generated output files.
+def create_case_directory(output_directory='', folder_name='design_case_', analysis_type=''):
+    """Creates a directory to hold each design's input files and generated output files.
 
     Args:
         output_directory (str): folder location for all generated results
         folder_name (str): folder naming convention
+        analysis_type (str): keyword for analysis software
 
     Returns:
         directory_path (str): location of the created case directory
@@ -19,13 +20,17 @@ def create_case_directory(output_directory='', folder_name='design_case_'):
     case_folder_path = os.path.join(output_directory, folder_name + str(folder_count))
 
     # Creates the case output folder if it does not already exist
-    assert not os.path.exists(case_folder_path), "Design output folder already exists"
+    assert not os.path.exists(case_folder_path), "Design output folder already exists"  # TODO: remove?
     os.makedirs(case_folder_path)
+    subdirectories = file_information(analysis_type)['subdirectories']
+    for directory in subdirectories:
+        subdirectory_path = os.path.join(case_folder_path, directory)
+        os.makedirs(subdirectory_path)
     os.chdir(case_folder_path)
     return case_folder_path
 
 
-def delete_case_files():  # TODO: create function to delete input and output files
+def delete_case_files():  # TODO: create function to delete input_files and output files that uses file extensions
     return
 
 
@@ -42,3 +47,34 @@ def parse_input(input_file_location):
     with open(input_file_location, 'r') as f:
         file_names = yaml.safe_load(f)
     return file_names
+
+
+def file_information(analysis_software_key):
+    """Returns file information for each analysis type.
+    'subdirectory' key contains a list of required subdirectory names in a design's folder
+    'input_extensions' key contains a list of input file extensions
+
+    """
+    analysis_software_key = analysis_software_key.lower()
+
+    file_info = {
+        'wamit': {'mesh_subdirectory': None,
+                  'bem_subdirectory': None,
+                  'subdirectories': [],
+                  'input_extensions': [],
+                  },
+        'nemoh': {'mesh_subdirectory': None,
+                  'bem_subdirectory': None,
+                  'subdirectories': [],
+                  'input_extensions': []
+                  },
+        'wec-sim': {'mesh_subdirectory': 'geometry',
+                    'bem_subdirectory': 'hydroData',
+                    'subdirectories': ['hydroData', 'geometry'],
+                    'input_extensions': []
+                    }
+    }
+
+    # TODO: update for NEMOH
+    # TODO: figure out how this should work for using both WAMIT and WEC-Sim
+    return file_info[analysis_software_key]
