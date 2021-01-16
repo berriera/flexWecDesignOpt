0fe653e07ec113a6737565bae43c2a2521be9506
Austin Berrier <berriera@oregonstate.edu>
2021-01-15 20:55:12 -0800

    Update analysis.py
    - Renamed run_wamit to run_analysis
    - Simplified run_analysis to run a list of manually entered raw string executable locations instead of attempting to account for conditionally running each type of analysis .exe files
    - Changed print statements to print what .exe is currently running
    - Changes directory to relative output folder path if needed

--- flexWecDesignOpt/analysis.py
+++ flexWecDesignOpt/analysis.py
@@ -1,31 +1,38 @@
-def run_wamit(bem_command, modes_command='', verbosity=True):
-    """
+def run_analysis(executable_locations, analysis_type, verbosity=True):
+    """ # TODO: update documentation
     This function runs the boundary element solver command in the current case directory.
 
     Args:
-        bem_command (str): location of the boundary element method executable
-        modes_command (str): location of the defmod executable
+        executable_locations (list of raw str): location of boundary element method executable files
         verbosity (bool): specify if print statements are outputted to screen
     Returns:
         None
     """
     import os
     import subprocess
-    
-    if verbosity is True:
-        print('\tRunning BEM...')
-    assert os.path.isfile(bem_command), 'Boundary element solver .exe file not found'
-    subprocess.run([bem_command])
 
-    # Runs user created generalized body modes application and then reruns WAMIT with the created gdf.mod file
-    if not modes_command:
-        if verbosity is True:
-            print('\tRunning defmod...')
-        assert os.path.isfile(modes_command), 'defmod.exe file not found'
-        subprocess.run([modes_command])
-        if verbosity is True:
-            print('\tRerunning BEM...')
-        subprocess.run([bem_command])
+    import file_mgmt
+
+    if verbosity:
+        print('\tRunning analysis...')
+
+    current_folder = os.getcwd()
+    relative_bem_output_path = file_mgmt.file_information(analysis_type)['bem_subdirectory']
+    if relative_bem_output_path is not None:
+        absolute_bem_folder_path = os.path.join(current_folder, relative_bem_output_path)
+        os.chdir(absolute_bem_folder_path)
+
+    for analysis_executable in executable_locations:
+        if verbosity:
+            print('\tRunning ' + os.path.basename(analysis_executable))
+        assert os.path.isfile(analysis_executable), 'Analysis .exe file in location not found'
+        subprocess.run([analysis_executable])
+
+    if relative_bem_output_path is not None:
+        os.chdir(current_folder)
+
+    if verbosity:
+        print('\tDone.')
     return
 
 
@@ -49,13 +56,14 @@ def boundary_condition_frequency_solver(function_name, eigenvalue_count=1, h=1.0
                                             columns or smaller if less roots in the specified range were found
     """
 
-    from scipy.optimize import fsolve
     import numpy as np
+    import scipy.optimize
+
     x0 = reltol  # used to avoid division by zero errors in solving certain problems with fsolve
     found_eigenvalue_count = 0
     eigenvalues = np.zeros(eigenvalue_count, )
     while found_eigenvalue_count < eigenvalue_count and x0 < freq_limit:
-        root = fsolve(function_name, x0, extra_args)[0]
+        root = scipy.optimize.fsolve(function_name, x0, extra_args)[0]
         if root > eps and not np.any(abs(eigenvalues - root) / root < reltol):
             eigenvalues[found_eigenvalue_count] = root
             found_eigenvalue_count = found_eigenvalue_count + 1
@@ -64,80 +72,35 @@ def boundary_condition_frequency_solver(function_name, eigenvalue_count=1, h=1.0
     return nonzero_eigenvalues
 
 
-def boundary_condition_frequency_solver__multidimensional(function_name, eigenvalue_count=1, h=1.0, var_count=1,
-                                                          var_increment=0, bounds=None, freq_limit=100, reltol=1e-5,
-                                                          eps=1e-2):
-    """
-    This function finds a user-specified number of eigenvalue solutions to a device's characteristic equation.
+# TODO: rewrite to travel along function_name and make brackets so there's no need for h
 
-    Args:
-        function_name (object): name of the equation f to be solved; format of the function should return a value
-                                  f(x) for the equation f(x) = 0
-        eigenvalue_count (int): number of unique roots to f(x) = 0 be found
-        h (float): increment used to guess new roots; default is 1.0
-        var_count(int): dimension of the nonlinear system
-        var_increment (int): array index of the variable to be incremented by h; used for solving boundary conditions
-                            involving more than one variable
-        freq_limit (float): maximum allowable frequency to be found; used as a break in case of unsuccessful root
-                            finding or to only search a certain space; default is 100
-        reltol (float): relative tolerance used to find if a new found root is unique; default is 1e-5
-        eps(float): used to as a minimum limit on found roots to avoid solutions too close to 0; default is 1e-3
-
-    Returns:
-        nonzero_eigenvalues (numpy array): list of non-zero roots to the equation; shape is n rows by eigenvalue_count
-                                            columns or smaller if less roots in the specified range of frequencies were
-                                            found to the n dimensional system
-    """
-    # TODO: add multidimensional test functions
-    from scipy.optimize import fsolve
-    import numpy as np
-
-    x0 = np.zeros(var_count) + reltol
-    found_roots_count = 0
-    eigenvalues = np.zeros((eigenvalue_count, var_count))
-    i = 0
-    while found_roots_count < eigenvalue_count and x0[var_increment] < freq_limit:
-        if bounds is not None:
-            x0 = bounds[:, 0] + np.random.random(size=var_count, ) * bounds[:, 1]
-        x0[var_increment] = i * h
-        root = fsolve(function_name, x0)
-        if np.all(root - eps > np.zeros(var_count)) and not np.any(abs(eigenvalues - root) / root < reltol):
-            eigenvalues[found_roots_count, :] = root
-            found_roots_count = found_roots_count + 1
-        i = i + 1
-    if var_count == 1:
-        nonzero_eigenvalues = eigenvalues[eigenvalues != 0.0]
-    else:
-        nonzero_eigenvalues = eigenvalues[eigenvalues[:, var_increment].argsort()]
-    return nonzero_eigenvalues
-
-
-def evaluate_device(DeviceClass, design_variables):
+def evaluate_device(device_class, design_variables):
     # External import
     import os
 
     # Internal imports
     import write_input_files
     import mesh
-    import analysis  # TODO: fix this to not import own file
+    import analysis  # TODO: fix this to not import own file (move to new file entirely called evaluate.py)
     import file_mgmt
     import output
 
-    # Create output folder for device evaluation
-    working_folder = file_mgmt.create_case_directory(DeviceClass.output_file_directory)
-
     # Create a device object
-    device_object = DeviceClass(design_variables)
-    input_file_substitutions = device_object.substitutions()
-    mesh_geometry = device_object.geometry()
+    device_object = device_class(design_variables)
+    input_file_substitutions = device_object.substitutions
+    mesh_geometry = device_object.mesh
 
-    # Create input files in directory and then run WAMIT in that directory
+    # Create output folder for device evaluation
+    working_folder = file_mgmt.create_case_directory(output_directory=device_class.output_file_directory,
+                                                     analysis_type=device_class.analysis_type)
+
+    # Create input_files files in directory and then run specified analysis executables in that directory
     write_input_files.create_case_files(device_object.input_file_directory, input_file_substitutions)
-    mesh.create_mesh_file(mesh_geometry, device_object.name, device_object.gmsh_exe_location)
-    if not device_object.defmod_location:
-        analysis.run_wamit(device_object.run_wamit_command, modes_command=device_object.defmod_location)
-    else:
-        analysis.run_wamit(device_object.run_wamit_command)
+    mesh.create_mesh_file(device_object=device_object,
+                          verbosity=device_object.verbosity)
+    analysis.run_analysis(executable_locations=device_class.exe_locations,
+                          analysis_type=device_class.analysis_type,
+                          verbosity=device_object.verbosity)
 
     # Output logger
     output.write_dict_to_text_file(input_file_substitutions)
