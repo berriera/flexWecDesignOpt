e42353f6d80d122ea6099b9de954a6e48eae53d4
Austin Berrier <berriera@oregonstate.edu>
2021-01-15 20:27:13 -0800

    Update mesh.py
    - Now passes in device argument only that contains original function arguments
    - Navigates to meshing folder if one is required for analysis

--- flexWecDesignOpt/mesh.py
+++ flexWecDesignOpt/mesh.py
@@ -1,19 +1,33 @@
-def create_mesh_file(geometry, device_name, gmsh_exe_location, verbosity=True):
+def create_mesh_file(device_object, verbosity=True):
     # TODO: set default device_name = 'mesh_file'
     """This function creates the initial mesh file in .stl format using the software GMSH
 
     Args:
-        geometry (geometry object): generated from the device.geometry class from a set of design variables
-        device_name (str): name of the device
-        gmsh_exe_location (str): file location of the installed GMSH software (should end in 'gmsh')
+        device_object (obj): instance of a device class
         verbosity (bool): controls printing verbosity for writing mesh file
 
     Returns:
         None
 
     """
+    import os
     import pygmsh
 
+    import file_mgmt
+
+    geometry = device_object.mesh
+    device_name = device_object.name
+    gmsh_exe_location = device_object.gmsh_exe_location
+    analysis_type = device_object.analysis
+
+    current_folder = os.getcwd()
+    relative_meshing_folder_path = file_mgmt.file_information(analysis_type)['mesh_subdirectory']
+
+    if relative_meshing_folder_path is not None:
+        absolute_meshing_folder_path = os.path.join(current_folder, relative_meshing_folder_path)
+        os.chdir(absolute_meshing_folder_path)
+
+    assert os.path.isfile(gmsh_exe_location), 'GMSH.exe file in location not found'
     if verbosity:
         print('\tMeshing...')
     mesh = pygmsh.generate_mesh(geometry,
@@ -26,6 +40,9 @@ def create_mesh_file(geometry, device_name, gmsh_exe_location, verbosity=True):
     mesh_file_name = device_name + '.stl'
     mesh.write(mesh_file_name)
     _trim_mesh_below_waterline(device_name=device_name)
+
+    if relative_meshing_folder_path is not None:
+        os.chdir(current_folder)
 
     if verbosity:
         print('\tDone.')
@@ -57,6 +74,6 @@ def _trim_mesh_below_waterline(device_name):
     meshmagick.mmio.write_GDF(device_name + '.gdf', vertices, faces)
 
     # TODO: make writing mesh file BEM software dependent (WAMIT, NEMOH, Capytaine)
-    # TODO: user specified mesh symmetry arguments for quicker write and meshing times,
+    # TODO: user specified mesh symmetry arguments for quicker write and meshing times for WAMIT,
     #  need to change meshmagick for this
     return
