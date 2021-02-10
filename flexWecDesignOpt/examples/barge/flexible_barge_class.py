a01182cdbd515a59bd2f58ee8572b437f6d2c2d2
Austin Berrier <berriera@oregonstate.edu>
2021-02-09 18:43:51 -0800

    Update flexible_barge_class.py
    Rewrote to use functions instead of specific variables for substitutions and meshing

--- flexWecDesignOpt/examples/barge/flexible_barge_class.py
+++ flexWecDesignOpt/examples/barge/flexible_barge_class.py
@@ -1,24 +1,10 @@
 class FlexibleBarge(object):
-    import os
-
-    # output_directory = os.path.abspath(os.path.join('examples', 'output'))
-    gmsh_exe_location = r'C:\Users\13365\Documents\gmsh-4.5.6-Windows64\gmsh-4.5.6-Windows64\gmsh'
-    defmod_exe_location = os.path.abspath(r'defmod.exe')
-    run_wamit_command = r'C:\WAMITv7\wamit'
-    exe_locations = [run_wamit_command, defmod_exe_location, run_wamit_command]
-
-    input_file_directory = os.path.abspath(r'input_files')
-    output_file_directory = r'C:\Users\13365\Desktop\optimization_output'
-    verbosity = True
-
-    # TODO: build absolute file paths from relative path in separate file file_experimentation.py and then fix here
 
     def __init__(self, design_vars):
         import numpy as np
         import pygmsh
 
         self.name = 'barge'
-        self.analysis = 'wec-sim'
 
         # Define design variables from input array
         length = design_vars[0]
@@ -37,7 +23,7 @@ class FlexibleBarge(object):
 
         # Barge inertial properties
         mass = density * length * width * height
-        Cg = 0
+        self.Cg = 0
         kx = ((1 / 12) * (length ** 2 + height ** 2)) ** (1 / 2)
         ky = ((1 / 12) * (width ** 2 + height ** 2)) ** (1 / 2)
         kz = ((1 / 12) * (length ** 2 + width ** 2)) ** (1 / 2)
@@ -47,8 +33,8 @@ class FlexibleBarge(object):
 
         # Build entire mass matrix from inertial and modal mass matrices
         mass_matrix = np.array([mass, mass, mass, Ix, Iy, Iz])
-        modal_mass_matrix = 0.25 * mass * np.ones(shape=(mode_count,))
-        external_mass_matrix = np.diag(np.concatenate((mass_matrix, modal_mass_matrix), axis=0))
+        modal_mass_matrix = 0.25 * mass * np.ones(shape=(mode_count, ))
+        self.external_mass_matrix = np.diag(np.concatenate((mass_matrix, modal_mass_matrix), axis=0))
 
         # Build entire stiffness matrix
         stiffness_matrix = np.zeros(shape=(6,))
@@ -57,24 +43,36 @@ class FlexibleBarge(object):
         modal_frequencies = (((elasticity_modulus * inertia_area) / (density * cross_section_area)) * (kappa ** 4)) ** (
                     1 / 2)
         modal_stiffness_matrix = (modal_frequencies ** 2) * modal_mass_matrix
-        external_stiffness_matrix = np.diag(np.concatenate((stiffness_matrix, modal_stiffness_matrix), axis=0))
+        self.external_stiffness_matrix = np.diag(np.concatenate((stiffness_matrix, modal_stiffness_matrix), axis=0))
 
-        # Define substitution dictionary for filling out input files
-        self.substitutions = {'mass_matrix': external_mass_matrix,
-                              'stiffness_matrix': external_stiffness_matrix,
-                              'Cg': Cg
-                              }
+        self.length = length
+        self.width = width
+        self.height = height
+        self.elasticity_modulus = elasticity_modulus
+        self.density = density
 
-        # Define .stl file meshing
+    # Define substitution dictionary for filling out input files
+    def barge_substitutions(self):
+        return {'mass_matrix': self.external_mass_matrix,
+                'stiffness_matrix': self.external_stiffness_matrix,
+                'Cg': self.Cg
+                }
+
+    # Define .stl file meshing
+    def barge_geometry(self):
+        import pygmsh
+
         barge_geometry = pygmsh.opencascade.Geometry()
-        barge_geometry.add_box(x0=[-1/2 * length, -1/2 * width, -1/2 * height], extents=[length, width, height],
+        barge_geometry.add_box(x0=[-1 / 2 * self.length, -1 / 2 * self.width, -1 / 2 * self.height],
+                               extents=[self.length, self.width, self.height],
                                char_length=2.50)
-        self.mesh = barge_geometry
+        return barge_geometry
 
-    def objective(self, output_folder_path):  # TODO: finish objective function
-        import os
-        os.chdir(output_folder_path)
-        return
+    # Define objective function calculation
+    def calculate_objective(self, design_variables, output_folder_path):  # TODO: finish actual objective function
+
+        objective = self.length * self.width * self.height + 1e9 / self.elasticity_modulus - self.density ** 2
+        return objective
 
 
 class BargeWamitMesh(object):
