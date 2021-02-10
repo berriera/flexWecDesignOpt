5dc8c52b19bf0e5eb613f1c1249048e6a17c191d
Austin Berrier <berriera@oregonstate.edu>
2021-02-09 18:48:32 -0800

    Update write_input_files.py
    - Fixed np.array2string max_line_width arg from float to int
    - Added analysis arg to create_case_files function

--- flexWecDesignOpt/write_input_files.py
+++ flexWecDesignOpt/write_input_files.py
@@ -11,17 +11,21 @@ def substitute_variables_in_line(line_text, variables, file_name='', line_number
     Returns:
         line_text (str) : The substituted line text for the file
 
-    """
+    """  # TODO: comment code
     import numpy as np
+
+    # Search for ? symbols in a given line of an input file
     while line_text.find('?') != -1:
         substitution_indices = [position for position, character in enumerate(line_text) if character == '?']
+
+        # Check that each substitution variable has two surrounding question marks
         if len(substitution_indices) % 2 != 0.0:
             raise ValueError("Check line number " + str(line_number) + " in file  " + file_name +
                              " for proper substitution formatting")
         string_substitution = line_text[substitution_indices[0]:substitution_indices[1] + 1]
         if variables is None:  # TODO: remove line after changing substitution file list
             return
-        elif isinstance(variables, np.ndarray):
+        elif isinstance(variables, np.ndarray):  # TODO: remove array substitution functionality
             # TODO: check if var is in dict or if array is out of bounds
             replacement_variable_number = int(line_text[substitution_indices[0] + 1: substitution_indices[1]])
             replacement_string = str(variables[replacement_variable_number - 1])
@@ -29,18 +33,23 @@ def substitute_variables_in_line(line_text, variables, file_name='', line_number
             try:  # TODO: replace try catch with assertion errors
                 replacement_variable_key = line_text[substitution_indices[0] + 1: substitution_indices[1]]
                 replacement_variable_value = variables[replacement_variable_key]
+
+                # Substitute real number in line
                 if isinstance(replacement_variable_value, int) or isinstance(replacement_variable_value, float):
                     replacement_string = str(variables[replacement_variable_key])
+
+                # Substitute 1d or 2d np array in input file
                 elif isinstance(replacement_variable_value, np.ndarray):
                     if replacement_variable_value.ndim == 1:
-                        array_string = np.array2string(replacement_variable_value, max_line_width=1e4)
+                        array_string = np.array2string(replacement_variable_value, max_line_width=10000)
                         replacement_string = array_string[1:-1]
+
                     elif replacement_variable_value.ndim == 2:
                         replacement_string = ""
                         row_count = replacement_variable_value.shape[0]
                         for row in range(row_count):
                             matrix_row = replacement_variable_value[row]
-                            matrix_row_string = np.array2string(matrix_row, max_line_width=1e4)
+                            matrix_row_string = np.array2string(matrix_row, max_line_width=10000)
                             replacement_string = replacement_string + matrix_row_string[1:-1]
                             if row != row_count - 1:
                                 replacement_string = replacement_string + '\n'
@@ -79,7 +88,7 @@ def change_case_file(text_file, design_var):
         return line_list
 
 
-def create_case_files(common_file_directory, substitution_array):
+def create_case_files(common_file_directory, substitution_array, analysis_type=''):  # TODO: delete?
     """Copies original input files from their common directory, and changes them in the process using variable
     substitution.
 
@@ -93,9 +102,19 @@ def create_case_files(common_file_directory, substitution_array):
 
     import shutil
     import os
+
+    import file_mgmt
+
     extension_copy_list = ['.pot', '.gdf', '.frc', '.spl', '.wam', '.cfg', '.ms2',
                            '.csf', '.bpi', '.rao', '.dmp', '.txt']
     # TODO: change extension list to passing in list of file names to be altered
+
+    current_folder = os.getcwd()
+    relative_bem_folder_path = file_mgmt.file_information(analysis_type)['bem_subdirectory']
+
+    if relative_bem_folder_path is not None:
+        absolute_bem_folder_path = os.path.join(current_folder, relative_bem_folder_path)
+        os.chdir(absolute_bem_folder_path)
 
     # File extension copy list used in accordance with WAMIT input file list (Chapter 4 of WAMIT user manual)
     files = os.listdir(common_file_directory)
@@ -109,4 +128,7 @@ def create_case_files(common_file_directory, substitution_array):
             with open(change_file, 'w') as f:
                 for new_string in new_file_text:
                     f.write(new_string)
+
+    if relative_bem_folder_path is not None:
+        os.chdir(current_folder)
     return
